2fe527d5afe81ccf48f57873dd5a15d47126497f
Sam <135857467+sm113@users.noreply.github.com>
2025-02-15 01:54:15 +0000

    finished

--- app.rb
+++ app.rb
@@ -5,33 +5,36 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
-
+  @second_num = params.fetch("second_num").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = (@second_num - @first_num).to_f
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_number = params.fetch("first_num").to_f
+  @second_number = params.fetch("second_num").to_f
+  @result = (@first_number * @second_number).to_f
   erb(:mult_result)
 end
 
@@ -40,5 +43,8 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @nume = params.fetch("numerator").to_f
+  @denom = params.fetch("denominator").to_f
+  @result = (@nume / @denom).to_f
   erb(:div_result)
 end
